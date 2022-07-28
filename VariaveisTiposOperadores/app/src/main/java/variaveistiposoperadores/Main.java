4b5df7a21b05c95b406603cc80a311623ad02664
valterjoaosilva <valterjoaosilva@gmail.com>
2022-07-28 20:51:46 -0300

    projetos java

---
+++ VariaveisTiposOperadores/app/src/main/java/variaveistiposoperadores/Main.java
@@ -0,0 +1,29 @@
+package variaveistiposoperadores;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     //Variáveis 
+
+     int idade = 0;
+     // float e double sempre que iniciar a variável, indicar seu tipo ao final da variável
+     float cotacaoDolar = 5.0f;
+     double cotacaoEuro = 6.0d;
+     //Tipo char armazena apenas uma letra usar aspas simples 
+     char genero = 'M';
+     byte placar = 30; 
+     boolean estaCadastrado = false;
+
+     //primitivos
+     String nome = "Carlos";
+
+     
+
+
+
+
+       
+    }
+    
+}
